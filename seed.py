af2e0c152ba234619e97e12684fa8b4feaa68764
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-08-10 18:11:29 +0300

    Issue #1: Add quotes from all other pages

--- seed.py
+++ seed.py
@@ -1,44 +1,72 @@
+from collections import defaultdict
 from json import dump
+from re import search
 
 from bs4 import BeautifulSoup
 from requests import get
 
 
 def main() -> None:
-    if (response := get('https://quotes.toscrape.com')).status_code == 200:
-        ATTRIBUTES = {
-            field: {key: field for key in ('class', 'itemprop')}
-            for field in ('author', 'text')
-        }
+    DOMAIN = 'https://quotes.toscrape.com'
 
-        nodes = BeautifulSoup(response.text, 'lxml').find_all(
+    ATTRIBUTES = {
+        field: {key: field for key in ('class', 'itemprop')}
+        for field in ('author', 'text')
+    }
+
+    results = defaultdict(list)
+    path = '/'
+
+    while True:
+        if (response := get(DOMAIN + path)).status_code != 200:
+            break
+
+        page = BeautifulSoup(response.text, 'lxml')
+
+        nodes = page.find_all(
             'div',
             {
                 'class': 'quote',
                 'itemtype': 'http://schema.org/CreativeWork'
-            }
+            },
         )
-
-        quotes = []
 
         for quote in nodes:
             phrase = quote.find('span', ATTRIBUTES['text'])
 
             wrapper = phrase.find_next_sibling('span')
-            author = wrapper.find('small', ATTRIBUTES['author']).text
+            author_name = wrapper.find('small', ATTRIBUTES['author']).text
+            author_link = wrapper.select('a[href^="/author/"]')[0]
 
-            if wrapper.text.replace('\n', '') == f'by {author}(about)':
-                tags = quote.find('div', class_='tags') \
-                    .select('a.tag[href^="/tag/"]')
+            author_info = f'by {author_name}{author_link.text}'
 
-                quotes.append({
-                    'tags': [tag.text for tag in tags],
-                    'author': author,
-                    'quote': phrase.text
-                })
+            if wrapper.text.replace('\n', '') != author_info:
+                continue
 
-        with open('quotes.json', 'w', encoding='utf-8') as file:
-            dump(quotes, file, indent=2, ensure_ascii=False)
+            results['authors'].append(author_link['href'])
+
+            tags = quote.find('div', class_='tags') \
+                .select('a.tag[href^="/tag/"]')
+
+            results['quotes'].append({
+                'tags': [tag.text for tag in tags],
+                'author': author_name,
+                'quote': phrase.text,
+            })
+
+        if (not (
+            (nodes := page.select('nav > ul.pager > li.next > a')) and
+            search(r'^/page/\d+/$', path := nodes[0]['href'])
+        )):
+            break
+
+    results['authors'] = list(set(results['authors']))
+
+    for type, result in results.items():
+        with open(f'{type}.json', 'w', encoding='utf-8') as file:
+            dump(result, file, indent=2, ensure_ascii=False)
+
+        print(f'Found {len(result)} {type}.')
 
 
 if __name__ == '__main__':
